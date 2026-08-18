7a6a30b037616714a77518329a502bc38101ff31
rjiang64 <rjiang64@wisc.edu>
2026-08-17 21:43:11 -0500

    implement sql with sqlalchemy

--- schemas.py
+++ schemas.py
@@ -1,4 +1,23 @@
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, EmailStr
+
+from datetime import datetime
+
+
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr =Field(min_length=1, max_length=120)
+    
+    
+class UserCreate(UserBase):
+    password: str = Field(min_length=6)
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    image_path: str | None
+    image_file: str | None
+
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
@@ -6,9 +25,10 @@ class PostBase(BaseModel):
     author: str = Field(min_length=1, max_length=50)
 
 class PostCreate(PostBase):
-    pass
+    user_id: int
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
     id: int
-    date_posted: str
+    date_posted: datetime
+    author: UserResponse
